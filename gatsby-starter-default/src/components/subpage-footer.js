43c59b718dc7e7721923eceb25b31136f9a8e89b
yuji fukuoka <1381320166@edu.k.u-tokyo.ac.jp>
2024-01-24 09:36:31 +0900

    use default starter

---
+++ gatsby-starter-default/src/components/subpage-footer.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const SubPageFooter = ({}) => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default SubPageFooter
